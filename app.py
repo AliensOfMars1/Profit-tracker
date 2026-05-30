05869112399c64e4bf339687aac8f75bf1b4a20f
CodesOfAnAlien <princemolly405@gmail.com>
2026-05-30 04:57:56 +0000

    idex updateed

--- app.py
+++ app.py
@@ -524,4 +524,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     
     # Use debug=False in production
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
